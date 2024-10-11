6a6880902905bf9f3ba2f04d7b90372b34772170
Serhii K <serhii.kozachenko.92@gmail.com>
2024-10-12 01:15:04 +0300

    Implemented first exercise (linked list & helpful functions)

---
+++ ex01.py
@@ -0,0 +1,164 @@
+from typing import Optional
+
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            cur = self.head
+            while cur.next:
+                cur = cur.next
+            cur.next = new_node
+
+    def insert_after(self, prev_node: Node, data):
+        if prev_node is None:
+            print("Попереднього вузла не існує.")
+            return
+        new_node = Node(data)
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
+    def delete_node(self, key: int):
+        cur = self.head
+        if cur and cur.data == key:
+            self.head = cur.next
+            cur = None
+            return
+        prev = None
+        while cur and cur.data != key:
+            prev = cur
+            cur = cur.next
+        if cur is None:
+            return
+        prev.next = cur.next
+        cur = None
+
+    def search_element(self, data: int) -> Optional[Node]:
+        cur = self.head
+        while cur:
+            if cur.data == data:
+                return cur
+            cur = cur.next
+        return None
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+    def reverse(self):
+        prev = None
+        current = self.head
+        while current:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+        self.head = prev
+
+    def merge_sort(self, head):
+        if not head or not head.next:
+            return head
+        
+        middle = self.get_middle(head)
+        next_to_middle = middle.next
+        middle.next = None
+
+        left = self.merge_sort(head)
+        right = self.merge_sort(next_to_middle)
+
+        sorted_list = self.sorted_merge(left, right)
+        return sorted_list
+
+    def sorted_merge(self, left, right):
+        if not left:
+            return right
+        if not right:
+            return left
+        
+        if left.data <= right.data:
+            result = left
+            result.next = self.sorted_merge(left.next, right)
+        else:
+            result = right
+            result.next = self.sorted_merge(left, right.next)
+        
+        return result
+
+    def get_middle(self, head):
+        if not head:
+            return head
+
+        slow = head
+        fast = head
+
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
+
+    def merge_sorted_lists(self, list1, list2):
+        dummy = Node(0)
+        tail = dummy
+
+        while list1 and list2:
+            if list1.data <= list2.data:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+
+        if list1:
+            tail.next = list1
+        else:
+            tail.next = list2
+
+        return dummy.next
+
+llist = LinkedList()
+llist.insert_at_beginning(5)
+llist.insert_at_beginning(10)
+llist.insert_at_beginning(15)
+llist.insert_at_end(20)
+llist.insert_at_end(25)
+
+print("Оригінальний список:")
+llist.print_list()
+
+llist.reverse()
+print("\nРеверсований список:")
+llist.print_list()
+
+llist.head = llist.merge_sort(llist.head)
+print("\nВідсортований список:")
+llist.print_list()
+
+llist2 = LinkedList()
+llist2.insert_at_end(1)
+llist2.insert_at_end(12)
+llist2.insert_at_end(18)
+
+merged_list = LinkedList()
+merged_list.head = merged_list.merge_sorted_lists(llist.head, llist2.head)
+print("\nОб'єднаний відсортований список:")
+merged_list.print_list()
